82d9c0b4daffb1ffd452158c015bda3dfb25f072
Harshit Shetty <shettyharshit5@gmail.com>
2018-02-21 12:23:34 +0530

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles do
-  	resources :comments
-  end
-
+  devise_for :users
   get 'welcome/index'
+ 
+  resources :articles do
+    resources :comments
+  end
+ 
   root 'welcome#index'
 end
