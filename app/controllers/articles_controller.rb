4dd89bc3d7735e59de2fab73499ef0a6f7ca8263
Harshit Shetty <shettyharshit5@gmail.com>
2018-02-21 12:32:26 +0530

    Logged in notification added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
-	http_basic_authenticate_with name: "kira", password: "hellokira", except: [:index, :show]
+	# http_basic_authenticate_with name: "kira", password: "hellokira", except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
 	# before_filter :authenticate_user!
 
 	def index
