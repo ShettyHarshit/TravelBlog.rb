1dd4706c03fb489b5e9fd78c346c3702c7f7c627
Harshit Shetty <swaharsh@gmail.com>
2018-03-30 22:42:17 +0530

    Change [Languages -> Location]

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226195114) do
+ActiveRecord::Schema.define(version: 20180330170744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "project"
-    t.string "languages"
+    t.string "location"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
